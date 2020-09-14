e4ee74ea33c708bc1bec68a43314414db46d9d76
tjohnson-scottlogic <tjohnson@scottlogic.com>
2020-09-14 12:22:50 +0100

    Added comment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.printf("Hello World!%n");
+	// Hello World
+        System.out.printf("Hello World!%n");
     }
 }
