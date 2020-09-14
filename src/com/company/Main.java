917602176ee188fdfd844896e7cf6d761f22e3ae
tjohnson-scottlogic <tjohnson@scottlogic.com>
2020-09-14 14:41:30 +0100

    Added a couple of lines

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// Hello World
         System.out.printf("Hello World!%n");
+        System.out.printf("How's it hanging?%n");
+        System.out.printf("Nice day isn't it?%n");
     }
 }
